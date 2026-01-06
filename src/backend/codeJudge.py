bce9bf1dcf33229b850468a7f2a226b1e8babb88
moli <kyeung516@gmail.com>
2026-01-06 18:30:31 +0800

    bug fix

--- src/backend/codeJudge.py
+++ src/backend/codeJudge.py
@@ -2,31 +2,7 @@ from pathlib import Path
 import json
 import copy
 import dataStructure as ds
-
-def loadInput(id):
-    folder = Path("instance") / id
-    print(folder)
-    for txt in folder.glob("*.txt"):
-        if txt.name == "in.txt":
-            with open(txt, "r", encoding="utf-8") as f:
-                loadedList = eval(f.read())
-                return loadedList
-                
-def loadOutput(id):
-    folder = Path("instance/") / id
-    print(folder)
-    for txt in folder.glob("*.txt"):
-        if txt.name == "out.txt":
-            with open(txt, "r", encoding="utf-8") as f:
-                loadedList = eval(f.read())
-                return loadedList
-            
-def getExecName(id):
-    di = Path("instance/") / id
-    for json_file in di.glob('*.json'):
-        with open(json_file, 'r', encoding="utf-8") as f:
-            data = json.load(f)
-            return data['problem']['content']['execName']
+import fetchProblem
         
 def execFunction(code_string, function_name, args=None, kwargs=None):
     if args is None:
@@ -47,11 +23,13 @@ def isbBuiltinClass(obj):
     return module_name == 'builtins'
 
 def judge(code, id):
-    stdin = loadInput(id)
-    stdout = loadOutput(id)
+    problem = fetchProblem.Problem(id)
+    stdin = problem.getInNOut(fetchProblem.INNAME)
+    stdout = problem.getInNOut(fetchProblem.OUTNAME)
+    execName = problem.getExecName()
     for i in range(len(stdin)):
         currentInput = copy.deepcopy(stdin[i])
-        output = execFunction(code, getExecName(id), stdin[i])
+        output = execFunction(code, execName, stdin[i])
         if isinstance(output, Exception):
             return False, f"Error occurred: {str(output)} At test case {i + 1} / {len(stdin)}\nInput: {tuple(map(lambda x: repr(x) if not(isbBuiltinClass(x)) else x, currentInput))}"
         if output != stdout[i]:
